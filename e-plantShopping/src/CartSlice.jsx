74326412946e8d9d7271254b5caabce24e63b4c3
KosteckHD <michalkoscianek@op.pl>
2026-03-19 13:48:26 +0100

    first version

---
+++ e-plantShopping/src/CartSlice.jsx
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [], // Initialize items as an empty array
+  },
+  reducers: {
+    addItem: (state, action) => {
+      const plant = action.payload;
+      const existingItem = state.items.find((item) => item.name === plant.name);
+
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({
+          ...plant,
+          quantity: 1,
+        });
+      }
+    },
+    removeItem: (state, action) => {
+    },
+    updateQuantity: (state, action) => {
+
+    
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+
+export default CartSlice.reducer;
